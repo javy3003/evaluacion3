6931c0d40e3ee0a00445132f0842dbabc4129b5b
javy3003 <jdonosa@uc.cl>
2025-10-24 13:15:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const portfolio = document.querySelector("#porotito");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+        let resultado = await consulta.json();
+        let trabajos = resultado.data;
+        console.log(trabajos);
+        trabajos.forEach((trabajo) => {
+            portfolio.innerHTML += `
+
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                <img src="${trabajo.imagen}" class"card-img-top">
+                                <div class="card-body">
+                                <p class="card-text">${trabajo.titulo}</p>
+                                <div class="d-flex
+                                justify-content-between
+                                align-items-center">
+                                <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.categoria}</button>
+                                </div>
+                                <small class="text-body-secondary"> ☆ ☆ ☆</small>
+             </div>
+             </div>
+            </div>
+            </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:", error);
+    }
+}
+
+datos("https://api.myjson.online/v1/records/b0d4073d-220a-4cf2-ae65-7cc3fd54f00e");
+
+
